74073f48506aa668c48bdb2d08a3eaf150b4ec0e
Amnon Ophir <ophiramnon@gmail.com>
2020-06-23 00:36:29 +0300

    code improvement
    TODO - editting, refractoring

--- ML_ex6.py
+++ ML_ex6.py
@@ -270,7 +270,7 @@ def section_one():
         params_values, accuracy_history = train(np.transpose(x_set), np.transpose(y_set), NN_1_ARCHITECTURE, 2000, 2) #etta=2
 
     x = np.arange(1, 2001)
-    plt.title("Square error per iteration, section a")
+    plt.title("Square error per iteration, section A")
     plt.xlabel("iteration")
     plt.ylabel("square error")
     plt.plot(x, s_error_history)
@@ -282,7 +282,7 @@ def section_two():
         params_values, accuracy_history = train(np.transpose(x_set), np.transpose(y_set), NN_2_ARCHITECTURE, 2000, 2) #etta=2
 
     x = np.arange(1, 2001)
-    plt.title("Square error per iteration, section a")
+    plt.title("Square error per iteration, section B")
     plt.xlabel("iteration")
     plt.ylabel("square error")
     plt.plot(x, s_error_history)
